38228eafc23db95b61c1e1287e7ce69d6d07e66f
fromitive <fromitive@gmail.com>
2024-10-16 17:33:36 +0900

    feat: 헬스체크 기능 구현

--- launch_next_container.sh
+++ launch_next_container.sh
@@ -4,9 +4,11 @@ GITHUB_SHA=$1
 PROFILE_ACTIVE=$2
 DOCKERHUB_USER_NAME=$3
 DOCKER_IMAGE_NAME=$4
+
 INITIAL_BLUE_GREEN_NETWORK_NAME="blue_green_network"
 BLUE_CONTAINER="chongdae_backend_blue"
 GREEN_CONTAINER="chongdae_backend_green"
+NGINX_CONTAINER="nginx"
 
 # parameter check
 if [ -z "$GITHUB_SHA" ] || [ -z "$PROFILE_ACTIVE" ] || [ -z "$DOCKERHUB_USER_NAME" ] || [ -z "$DOCKER_IMAGE_NAME" ]; then
@@ -15,7 +17,7 @@ if [ -z "$GITHUB_SHA" ] || [ -z "$PROFILE_ACTIVE" ] || [ -z "$DOCKERHUB_USER_NAM
 fi
 
 get_active_container() {
-  local active_container_name=$(docker exec nginx cat /etc/nginx/conf.d/default.conf | \
+  local active_container_name=$(docker exec ${NGINX_CONTAINER} cat /etc/nginx/conf.d/default.conf | \
     grep -P '^\s*server\s+\S+:' | \
     grep -oP '(?<=server\s)[^:;]+' | \
     sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
@@ -38,6 +40,22 @@ get_next_container() {
   return 1
 }
 
+health_check() {
+	local next_container=$1
+	local sleep_seconds="10s"
+	
+	echo "[INFO] sleep $sleep_seconds for waiting container up"
+	sleep $sleep_seconds
+
+	local status=$(docker exec ${NGINX_CONTAINER} curl -H "Host: localhost" -o /dev/null -s -w "%{http_code}\n" http://$next_container:8080)
+
+	if [ $? -eq 0 ]; then
+		echo "server response code: $status"
+		return 0
+	fi
+	return 1
+}
+
 ACTIVE_CONTAINER=$(get_active_container)
 NEXT_CONTAINER=$(get_next_container $ACTIVE_CONTAINER)
 
@@ -51,6 +69,8 @@ docker run -d \
 	-v /logs:/logs \
 	-e SPRING_PROFILES_ACTIVE=${PROFILE_ACTIVE} \
 	${DOCKERHUB_USER_NAME}/${DOCKER_IMAGE_NAME}:${GITHUB_SHA:0:7}
+
+health_check $NEXT_CONTAINER
 
 if [ $? -ne 0 ]; then
 	exit 1
